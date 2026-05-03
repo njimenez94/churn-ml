10d6ec2425188dfb380941b3532714fab6de1a75
njimenez94 <jd.nicolas1@gmail.com>
2026-05-03 18:59:00 -0400

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Churn prediction for telecom using machine learning, feature engineering, and production-ready pipeline',
+    author='Nicolas Jimenez Diaz',
+    license='MIT',
+)
