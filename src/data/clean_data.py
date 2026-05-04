a12ec20458d65441f2448a9f3858c2c883063797
njimenez94 <jd.nicolas1@gmail.com>
2026-05-03 21:10:05 -0400

    eda completado

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,34 +1,11 @@
 import pandas as pd
 
-COLS_NO_SERVICE = [
-    'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
-    'TechSupport', 'StreamingTV', 'StreamingMovies',
-    'MultipleLines'
-]
-
-BINS_COL = [
-    'Churn',
-    'Partner', 'Dependents', 'PhoneService',
-    'MultipleLines',
-    'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
-    'TechSupport', 'StreamingTV', 'StreamingMovies',
-    'PaperlessBilling'
-]
-
-def _string_to_numeric(df, col):
+def _convert_total_charges(df, col='TotalCharges'):
     df[col] = pd.to_numeric(df[col], errors='coerce')
+    df[col] = df[col].fillna(0).clip(lower=0)
     return df
 
-def _clean_no_service(df, cols):
-    replace_map = {
-        'No internet service': None,
-        'No phone service': None
-    }
-    for col in cols:
-        df[col] = df[col].replace(replace_map)  
-    return df
-
-def _convert_binary_to_numeric(df, cols):
+def _convert_binary_to_numeric(df, cols= ['Churn']):
     for col in cols:
         df[col] = df[col].replace({'Yes': 1, 'No': 0})
     return df
@@ -36,9 +13,7 @@ def _convert_binary_to_numeric(df, cols):
 def clean_data(file_path):
     df = pd.read_csv(file_path)
     df = df.drop(columns=['customerID'])
-    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
-    df['TotalCharges'] = df['TotalCharges'].fillna(0).clip(lower=0)
-    df = _clean_no_service(df, COLS_NO_SERVICE)
-    df = _convert_binary_to_numeric(df, BINS_COL)
+    df = _convert_total_charges(df)
+    df = _convert_binary_to_numeric(df)
     print(f"Data cleaned. Shape: {df.shape}")
     return df
